0e3c8e2b639fffc27dc33bd61a2ffd41b9aa96d8
Joseph Becher <jwbecher@gmail.com>
2014-09-14 00:51:50 -0400

    Added localization and moved slash commands to seperate file.

--- npcBlocker.lua
+++ npcBlocker.lua
@@ -15,6 +15,11 @@
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+-- Require npcBlockerCommands.lua
+-- require "npcBlockerCommands";
+
+local _, L = ...;
+
     -- Create the table to hold the npcs we want to block says for
 local npcBlock_Defaults = {
   ["NPCs"] = {},
@@ -50,24 +55,12 @@ local function npcBlock_Init()
     table.insert(npcBlock_Defaults["NPCs"], "Smiling Jade");
     -- Celestial Court Brewmasters
     table.insert(npcBlock_Defaults["NPCs"], "Brewmaster Tzu");
+    -- Celestial Court Protectors
+    table.insert(npcBlock_Defaults["NPCs"], "Protector Yi");
+
 
 
 end
-
-function npcBlocker__dumpDB()
-
-   for i,npc in ipairs(npcBlock_Defaults["NPCs"]) do
-        print(npc);
-   end
-end
-
-function npcBlocker__reloadDB()
-
-    npcBlock_Defaults["NPCs"] = {};
-    npcBlock_Init()
-    DEFAULT_CHAT_FRAME:AddMessage("npcBlocker Options database reloaded.");
-end
-
 
 -- this is the chat filter that hides the chat messages we don't want.
 function npcBlocker__ChannelMsgFilterNPCSay(self, event, msg, author, ...)
@@ -89,20 +82,24 @@ function mainFrameEvents:ADDON_LOADED(arg1)
     if arg1 == "npcblocker" then
         if (not npcBlock_Options) then
             npcBlock_Options = npcBlock_Defaults["Options"];
-            DEFAULT_CHAT_FRAME:AddMessage("npcBlocker Options database not found. Generating...");
+            DEFAULT_CHAT_FRAME:AddMessage(L["npcBlocker Options database not found. Generating..."]);
         end
 
+        if next(npcBlock_Defaults["NPCs"]) == nil then
+            DEFAULT_CHAT_FRAME:AddMessage(L["npcBlocker NPC database needs to be rebuilt..."]);
+            npcBlock_Init();
+        end
 
         if npcBlockerStatus == nil then
             npcBlockerStatus = true;
         end
-        if next(npcBlock_Defaults["NPCs"]) == nil then
-            DEFAULT_CHAT_FRAME:AddMessage("npcBlocker NPC database needs to be rebuilt...");
-            npcBlock_Init();
+
+        if npcBlockerStatus == true then
+            addChatFilters();
         end
 
         -- Our saved variables are ready at this point. If there are none, both variables will set to nil.
-        print("NPC Blocker Loaded.");
+        print(L["NPC Blocker Loaded."]);
     end
 end
 
@@ -115,41 +112,37 @@ for k, v in pairs(mainFrameEvents) do
  mainFrame:RegisterEvent(k); -- Register all events for which handlers have been defined
 end
 
--- add chat filter for npc says
-ChatFrame_AddMessageEventFilter("CHAT_MSG_MONSTER_SAY", npcBlocker__ChannelMsgFilterNPCSay);
--- add chat filter for player yells
-ChatFrame_AddMessageEventFilter("CHAT_MSG_YELL", npcBlocker__ChannelMsgFilterNPCSay);
-
---------------------------------------------------------------------------------
--- Slash command handler function
-function npcBlocker__slashCmdHandler(opcode, editbox)
-    if npcBlockerStatus == true then
-        npcBlockerStatus = false;
-        -- add chat filter for npc says
-        ChatFrame_RemoveMessageEventFilter("CHAT_MSG_MONSTER_SAY", npcBlocker__ChannelMsgFilterNPCSay);
-        -- add chat filter for player yells
-        ChatFrame_RemoveMessageEventFilter("CHAT_MSG_YELL", npcBlocker__ChannelMsgFilterNPCSay);
-        print("NPC Blocker is now off.");
-    else
-       	npcBlockerStatus = true;
-        -- add chat filter for npc says
-        ChatFrame_AddMessageEventFilter("CHAT_MSG_MONSTER_SAY", npcBlocker__ChannelMsgFilterNPCSay);
-        -- add chat filter for player yells
-        ChatFrame_AddMessageEventFilter("CHAT_MSG_YELL", npcBlocker__ChannelMsgFilterNPCSay);
-	print("NPC Blocker is now on.");
-    end
+----------------------------------------------------
+-- Remove the chat filters
+function removeChatFilters()
+    -- add chat filter for npc says
+    ChatFrame_RemoveMessageEventFilter("CHAT_MSG_MONSTER_SAY", npcBlocker__ChannelMsgFilterNPCSay);
+    -- add chat filter for player yells
+    ChatFrame_RemoveMessageEventFilter("CHAT_MSG_YELL", npcBlocker__ChannelMsgFilterNPCSay);
+    print(L["NPC Blocker is now off."]);
 end
+
+----------------------------------------------------
+-- Add the chat filters
+function addChatFilters()
+    -- add chat filter for npc says
+    ChatFrame_AddMessageEventFilter("CHAT_MSG_MONSTER_SAY", npcBlocker__ChannelMsgFilterNPCSay);
+    -- add chat filter for player yells
+    ChatFrame_AddMessageEventFilter("CHAT_MSG_YELL", npcBlocker__ChannelMsgFilterNPCSay);
+    print(L["NPC Blocker is now on."]);
+end
+
 
 --------------------------------------------------------------------------------
 -- Create the slash command
-SLASH_NPCBLOCKER1 = '/npcblock';
-SLASH_NPCBLOCKER_DUMP1 = '/npcblock_dump';
-SLASH_NPCBLOCKER_RELOAD1 = '/npcblock_reload';
+SLASH_NPCBLOCKER_TOGGLESTATUS1 = '/npcblock';
+SLASH_NPCBLOCKER_DUMPDATABASE1 = '/npcblock_dump';
+SLASH_NPCBLOCKER_RELOADDATABASE1 = '/npcblock_reload';
 
 -- Assign slash command to the handler function
-SlashCmdList["NPCBLOCKER"] = npcBlocker__slashCmdHandler;
-SlashCmdList["NPCBLOCKER_DUMP"] = npcBlocker__dumpDB;
-SlashCmdList["NPCBLOCKER_RELOAD"] = npcBlocker__reloadDB;
+SlashCmdList["NPCBLOCKER_TOGGLESTATUS"] = npcBlocker__slashToggleStatus;
+SlashCmdList["NPCBLOCKER_DUMPDATABASE"] = npcBlocker__slashDumpDatabase;
+SlashCmdList["NPCBLOCKER_RELOADDATABASE"] = npcBlocker__slashReloadDatabase;
 
 
 
